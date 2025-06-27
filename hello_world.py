546645b19c0d86745a7222679ae708c357cc50d8
Adrian Luna <aluna.al791@gmail.com>
2025-06-26 21:27:53 -0700

    median branch

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,15 @@
 print("Adios, World!")
+
+import math
+
+def median(input):
+  n = len(input)
+  if n % 2 == 1:
+    mid = input[math.floor(n/2)]
+  else:
+    mid_num = [n/2, n/2 - 1]
+    mid = mean(mid_num)
+  return mid
+
+
+
